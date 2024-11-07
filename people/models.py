2f495cd0d0c20bcdce60910f3046ff1245353a97
Bram Lefebvre <bramlefebvre@gmail.com>
2024-11-07 15:06:05 +0100

    persons json

--- people/models.py
+++ people/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     age = models.IntegerField()
 
-    
+    def __str__(self):
+        return "name = " + self.name + "; age = " + str(self.age)
--- people/views.py
+++ people/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from people.models import Person
+import json
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the people index.")
+    name = request.GET.get("name")
+    age = request.GET.get("age")
+    print("name: " + name)
+    Person.objects
+    serialized_persons = get_persons()
+    persons_json = json.dumps(serialized_persons)
+    return HttpResponse(persons_json)
+
+
+
+def serialize_persons(persons):
+    serialized_persons = []
+    for person in persons:
+        serialized_person = {
+            'name': person.name,
+            'age': person.age
+        }
+        serialized_persons.append(serialized_person)
+    return serialized_persons
+
+def get_persons():
+    persons = Person.objects.all()
+    persons = persons.iterator()
+    serialized_persons = []
+    for person in persons:
+        serialized_person = {
+            'name': person.name,
+            'age': person.age
+        }
+        serialized_persons.append(serialized_person)
+    return serialized_persons
+    # return json.dumps(serialized_persons)
+
